04300dfe6fc402ef3a255c71c6b4f567d7bbef49
Server <milansuthar17@yahoo.com>
2020-12-31 17:34:48 +0530

    second

---
+++ common/config/params.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'adminEmail' => 'sutharmilan007@gmail.com',
+    'supportEmail' => 'sutharmilan007@gmail.com',
+    'senderEmail' => 'sutharmilan007@gmail.com',
+    'senderName' => 'milan',
+    'user.passwordResetTokenExpire' => 3600,
+    'user.passwordMinLength' => 8,
+];
